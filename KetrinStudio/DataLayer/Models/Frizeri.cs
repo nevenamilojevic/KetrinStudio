ca66bb122f9aacf54f06528dc22362c8221f0f52
DESKTOP-RM2PJPV\Ivana <25-2019@ftn.edu.rs>
2022-01-14 21:07:17 +0100

    DataLayer

---
+++ KetrinStudio/DataLayer/Models/Frizeri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+     public class Frizeri
+    {
+        public int Id_Frizer { get; set; }
+        public String BrojOsiguranja { get; set; }
+        public String ImeF { get; set; }
+        public String PrezimeF { get; set; }
+        public String Adresa { get; set; }
+        public String BrojTelefonaF { get; set; }
+    }
+}
---
+++ KetrinStudio/DataLayer/Models/Klijenti.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Klijenti
+    {
+        public int Id_Klijent { get; set; }
+        public String ImeK { get; set; }
+        public String PrezimeK { get; set; }
+        public  String BrojTelefonaK { get; set; }
+        public String Email { get; set; }
+        public DateTime DatumTretaman { get; set; }
+
+    }
+}
