b6d3a868ccdafaf7cfd18dc9c715bb8c77e36907
DESKTOP-7I97I2V\Nemanja <nevenamilojee@gmail.com>
2022-01-14 20:42:39 +0100

    BusinessLayer

---
+++ KetrinStudio/BusinessLayer/VrsteBusiness.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+     public class VrsteBusiness
+    {
+        readonly VrstaRepository vrstaRepositoriy = new VrstaRepository();
+        public List<Vrste> GetAllVrste()
+        {
+
+            return vrstaRepositoriy.GetAllVrste();
+
+        }
+        
+    }
+}
